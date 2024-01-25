e27643616c97607b062519457a1d22b5b3002ad0
Harsh0592 <harshdalasaniya@gmail.com>
2024-01-25 10:11:42 +0530

    Done

---
+++ Assignment/Flutter_Assesment/database/lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:database/Screens/Dictionary.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DictionaryScreen(),
+    );
+  }
+}
